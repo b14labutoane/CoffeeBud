edbe331e977c9d3197557fa24a6bc0e86bb3d05a
bia <biancabadescu@gmail.com>
2025-03-14 21:30:52 +0200

    startup commit

---
+++ client/src/Signup.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+
+function Signup(){
+    
+    return (
+        <div className = "d-flex justify-conent-center align-items-center bg-secondary vh-100">
+            <div className = "bg-white p-3 rounded w-25">
+                <h2>Register</h2>
+                <form>
+                    <div className = 
+                </form>
+            </div>
+        </div>
+    )
+
+}
