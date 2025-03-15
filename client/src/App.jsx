5fba493eb9968669cd25e4d690b3164d0ad27a95
bia <biancabadescu@gmail.com>
2025-03-15 16:55:24 +0200

    megacommit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import Login from "./components/login.jsx"
+import Register from "./components/register.jsx"
+import { useState } from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-const App = () => (
-  <div className="App">
-    Hello World
-  </div>
-);
+function App() {
+    return (
+    <BrowserRouter>
+        <Routes>
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+            </Routes>
+    </BrowserRouter>,
+    <div className="App">
+        Hello World
+     </div>
+    )
+}
 
-
-export default App
+export default App;
---
+++ server/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://bia_bud:gsabrupeprogramarea@coffeebuddb.7j0fi.mongodb.net/?retryWrites=true&w=majority&appName=CoffeeBudDb',    
+    jwtSecret: 'hello'
+}
