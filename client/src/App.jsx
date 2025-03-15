d7f64f1cecd73abfc1bf8a798d05f9e3d7aed723
bia <biancabadescu@gmail.com>
2025-03-15 13:12:09 +0200

    MERGE FRONTENDU

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  console.log("App component rendered");
-  return (
-    <div>
-      Hello World
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    Hello World
+  </div>
+);
 
-export default App;
+
+export default App
